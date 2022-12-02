009168ee131f02188e6f8817c850b7f9cc7ae337
suntong <suntong@cpan.org>
2022-12-02 02:20:52 +0000

    - [-] clean up

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,22 +2,9 @@
 export default (props) => {
     return (
       <header className="App-header">
-      <img src="Octocat.png" className="App-logo" alt="logo" />
+      <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" className="App-logo" alt="logo" />
       <p>
       {props.name} <span className="heart">♥️</span> React
-      </p>
-      <p className="small">
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </p>
     </header>
         )
